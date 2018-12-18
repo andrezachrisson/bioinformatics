c57f626274cbaa81e4fd8cc56c19b0e63477ed58
christopherrrons <christopherherron09@gmail.com>
2018-12-18 16:54:22 +0100

    Fixade så allt är i funktioner. Just  flyttas filerna till tempdir men resten funkar.

--- Main.py
+++ Main.py
@@ -11,34 +11,63 @@ from dendropy import Tree
 import tempfile
 from subprocess import Popen, PIPE
 import os
+import re
 
+def fastPhylo():
+    dirpath = os.path.dirname(os.path.realpath(__file__))
+    with tempfile.TemporaryDirectory(dir = dirpath) as tempdir:
+
+        """ Reduced noise tree"""
+        with open('red_fast', 'w') as file:
+            process = Popen(['fastprot','reduced_file'], stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding = 'utf8')
+            file.write(process.stdout.read())
+            os.rename("red_fast", tempdir + "/"+ "red_fast")
+        #pdb.set_trace()
+
+        with open('red_tree', 'w') as file:
+            process = Popen(['fnj','red_fast', '-O', 'newick'], stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding = 'utf8', cwd = tempdir)
+            file.write(process.stdout.read())
+            os.rename("red_tree", tempdir + "/"+ "red_tree")
+        #pdb.set_trace()
+
+        """ Normal tree"""
+        with open('normal_fast', 'w') as file:
+            process = Popen(['fastprot','s001.align.1.msl'], stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding = 'utf8')
+            file.write(process.stdout.read())
+            os.rename("normal_fast", tempdir + "/"+ "normal_fast")
+        #pdb.set_trace()
+
+        with open('normal_tree', 'w') as file:
+            process = Popen(['fnj','normal_fast', '-O', 'newick'], stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding = 'utf8', cwd = tempdir)
+            file.write(process.stdout.read())
+            os.rename("normal_tree", tempdir + "/"+ "normal_tree")
+        #pdb.set_trace()
+
+        tree_compare (tempdir)
+
+def tree_compare(tempdir):
+        tns = dendropy.TaxonNamespace()
+        tree1 = Tree.get_from_path(
+                "ref.tree",
+                "newick",
+                taxon_namespace=tns)
+        tree2 = Tree.get_from_path(
+                tempdir + "/"+ "normal_tree",
+                "newick",
+                taxon_namespace=tns)
+        tree3 = Tree.get_from_path(
+                tempdir + "/"+ "red_tree",
+                "newick",
+                taxon_namespace=tns)
+        tree1.encode_bipartitions()
+        tree2.encode_bipartitions()
+        print(treecompare.symmetric_difference(tree1, tree2))
+        print(treecompare.symmetric_difference(tree1, tree3))
 
 def main():
-    read_file('s001.align.1.msl')
+    read_file("s001.align.1.msl")
+    fastPhylo()
 
-    tns = dendropy.TaxonNamespace()
-    tree1 = Tree.get_from_path(
-            "ref.tree",
-            "newick",
-            taxon_namespace=tns)
-    tree2 = Tree.get_from_path(
-            "normal_tree",
-            "newick",
-            taxon_namespace=tns)
-    tree1.encode_bipartitions()
-    tree2.encode_bipartitions()
-    print(treecompare.symmetric_difference(tree1, tree2))
-
-    dirpath = os.path.dirname(os.path.realpath(__file__))
-
-    with tempfile.TemporaryDirectory(dir = dirpath) as tempdir:
-        # fastprot fastafile
-            print(tempdir)
-            os.chdir(tempdir)
-            with open('red_fast', 'w') as file:
-                process = Popen(['fastprot','reduced_file'], stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding = 'utf8')
-                file.write(process.stdout.read())
-            pdb.set_trace()
 
 
 
